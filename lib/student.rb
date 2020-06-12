396af56424f4145ed51e8f8465b142f472302930
Annia Forney <anniaforney@gmail.com>
2020-06-12 14:27:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -105,10 +105,10 @@ class Student
     end
   end
 
-  def self.first_student_in_grade_10
+  def self.first_student_in_grade_10(grade)
   end
 
-  def self.all_students_in_grade_X
+  def self.all_students_in_grade_X(x)
   end
 
 end
