1acb31a45924b155b05d09b56a4d369ecdae24e2
Annia Forney <anniaforney@gmail.com>
2020-06-12 13:25:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,12 +67,44 @@ class Student
   end
 
   def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+      SQL
+
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end
   end
 
   def self.students_below_12th_grade
+    sql = <<-SQL
+
+      SELECT *
+      FROM students
+      WHERE grade < 12
+      LIMIT 1
+      SQL
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end
   end
 
   def self.first_X_students_in_grade_10
+    sql = <<-SQL
+
+    SELECT *
+    FROM students
+    WHERE grade = 10
+
+    LIMIT 1 
+
+    SQL
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end.first
+
   end
 
   def self.student_in_grade_10
