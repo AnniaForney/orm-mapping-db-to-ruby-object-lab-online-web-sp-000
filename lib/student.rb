830767e5d98899beded5b685152c97270175f038
Annia Forney <anniaforney@gmail.com>
2020-06-12 14:37:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -111,10 +111,10 @@ class Student
     SELECT *
     FROM students
     WHERE grade = 10
-     
+
     SQL
 
-    DB[:conn].execute(sql, student).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
       end
   end
