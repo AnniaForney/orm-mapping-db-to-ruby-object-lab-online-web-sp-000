bfa74b4dcefe9af56fe8c28df0edd2155f7fec05
Annia Forney <anniaforney@gmail.com>
2020-06-12 13:43:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -108,15 +108,6 @@ class Student
   end
 
   def self.student_in_grade_10
-    sql = <<-SQL
-
-    SELECT *
-    FROM students
-    ORDER BY (grade, x)
-
-    DB[:conn].execute(sql).map do |row|
-    self.new_from_db(row)
-    end.first 
   end
 
   def self.all_students_in_grade_X
