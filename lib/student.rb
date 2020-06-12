9b74f7651eb90da1c1ea1cc4c61b66f8478e8e6a
Annia Forney <anniaforney@gmail.com>
2020-06-12 13:51:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -96,11 +96,11 @@ class Student
 
     SELECT *
     FROM students
-    WHERE grade = ? 
+    WHERE grade = ?
     LIMIT 1
 
     SQL
-    DB[:conn].execute(sql, x).map do |row|
+    DB[:conn].execute(sql, grade).map do |row|
       self.new_from_db(row)
     end.first
 
